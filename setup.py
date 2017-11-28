a8978986fa1cd06195d6137cc3e750838c02e6d3
Icyblade Dai <icyblade.py@gmail.com>
2017-11-28 17:08:21 +0800

    add setup test

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='dream',
     version='0.1.0',
-    packages=find_packages(exclude=['tests', 'examples']),
+    packages=find_packages(exclude=['tests', 'examples', 'docs']),
     url='https://github.com/icyblade/dream',
     license='MIT',
     author='Icyblade Dai',
