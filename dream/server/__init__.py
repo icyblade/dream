6191d48145a35e79fee6d8047b686b082e8f2c2b
Icyblade Dai <icyblade.py@gmail.com>
2018-02-06 14:00:35 +0800

    update server

--- dream/server/__init__.py
+++ dream/server/__init__.py
@@ -1,9 +1,12 @@
 import json
 import os
+from importlib import import_module
 
 from flask import Flask
+from flask import request
 
-from .models import Agent, Config, db
+from .helpers import flask_method, helper
+from .models import Agent, Quota, db
 
 app = Flask('dream')
 
@@ -19,60 +22,153 @@ else:
 db.init_app(app)
 
 
-@app.route('/list', defaults={'page': 0})
-@app.route('/list/<page>')
-def list(page, page_size=20):
-    try:
-        page = int(page)
+@app.route('/api', methods=['GET', 'POST'])
+def api():
+    """Entry point."""
+    action = request.args.get('action', '')
+    if action == 'CREATE':
+        if request.method == 'POST':
+            return create(request)
+    elif action == 'DELETE':
+        if request.method == 'POST':
+            return delete(request)
+    elif action == 'GETALLLIST':
+        if request.method == 'GET':
+            return get_running_agents(request)
+    elif action == 'EMPTY':
+        if request.method == 'POST':
+            return empty(request)
+    elif action == 'GETQUOTALIST':
+        if request.method == 'GET':
+            return get_available_agents(request)
+    else:
+        pass
 
-        status = 'SUCCESS'
-        errCode = 0
-        message = ''
-        maximum_agent = int(Config.query.filter_by(key='maximum_agent').first().value)
-        n_current_agent = int(Agent.query.count())
-        n_pages = n_current_agent // page_size + 1
-        current_agents = Agent.query.offset(page * page_size).limit(page_size).all()
-        current_agents = [row.as_dict() for row in current_agents]
-        has_next_page = n_pages > page + 1
-    except Exception as e:
-        status = 'FAIL'
-        errCode = -1
-        message = str(e)
-        maximum_agent = 0
-        n_current_agent = 0
-        n_pages = 1
-        current_agents = []
-        has_next_page = False
+    return json.dumps({})
+
+
+@helper
+def get_nb_running_agents():
+    """Get numbers of running agents.
+
+    Returns
+    --------
+    dict
+        Mapping between agent type and numbers of running agents of this agent type.
+    """
+    return dict(db.session.query(
+        Agent.type, db.func.count(Agent.id)
+    ).group_by(Agent.type).all())
+
+
+@helper
+def get_nb_all_agents():
+    """Get quota of all available agents.
+
+    Returns
+    --------
+    dict
+        Mapping between agent type and quota of this agent type.
+    """
+    return dict(db.session.query(
+        Quota.type, Quota.maximum_quota
+    ).all())
+
+
+@helper
+def get_nb_available_agents():
+    """Get numbers of remaining agents.
+
+    Equals to quota of all agents minus numbers of running agents.
+
+    Returns
+    --------
+    dict
+        Mapping between agent type and numbers of remaining agents of this agent type.
+    """
+    nb_running_agents = get_nb_running_agents()
+    agent_quota = get_nb_all_agents()
+
+    result = {}
+    for agent_type, value in agent_quota.items():
+        if agent_type in nb_running_agents:
+            result[agent_type] = value - nb_running_agents[agent_type]
+        else:
+            result[agent_type] = value
+
+    return result
+
+
+@flask_method
+def get_running_agents(request):
+    """Get numbers of running agents."""
+    result = {}
+    for agent_type, value in get_nb_running_agents().items():
+        agent = getattr(import_module(f'dream.agent.{agent_type}'), 'Agent')
+        result[agent_type] = {
+            'numbers': value,
+            'EVRange': agent.ev_range,
+        }
+    return json.dumps({
+        'errcode': 0,
+        'AITypeList': result,
+    })
+
+
+@flask_method
+def get_available_agents(request):
+    """Get numbers of remaining agents."""
+    result = {}
+    for agent_type, value in get_nb_available_agents().items():
+        agent = getattr(import_module(f'dream.agent.{agent_type}'), 'Agent')
+        result[agent_type] = {
+            'numbers': value,
+            'EVRange': agent.ev_range
+        }
 
     return json.dumps({
-        'status': status,
-        'errCode': errCode,
-        'message': message,
-        'datas': {
-            'maximum_agents': maximum_agent,
-            'current_agents': current_agents,
-        },
-        'hasNextPage': has_next_page,
-        'totalPages': n_pages,
-        'totalCount': n_current_agent,
-    })+'\n'
+        'errcode': 0,
+        'AITypeList': result,
+    })
 
 
-@app.route('/get/<name>')
-def get(name):
-    raise NotImplementedError
+@flask_method
+def create(request):
+    table_id = request.get_json()['tableid']
+    position = request.get_json()['position']
+    ai_type = request.get_json()['AIType']
+
+    assert position.isdigit()
+
+    assert 0 <= int(position) <= 8
+
+    quota = get_nb_available_agents()
+    assert str(ai_type) in quota
+    assert quota[str(ai_type)] >= 0
+
+    agent = Agent(type=ai_type, name=ai_type, port=41488)  # TODO
+    db.session.add(agent)
+    db.session.commit()
+
+    return json.dumps({
+        'errcode': 0,
+        'tableid': table_id, 'position': position,
+        'port': agent.port, 'address': f'tcp://{app.config["SERVER_ADDRESS"]}:{agent.port}',
+        'AIType': ai_type, 'AIID': agent.id,
+    })
 
 
-@app.route('/create/<name>')
-def create(name):
-    raise NotImplementedError
+@flask_method
+def delete(request):
+    ai_id = request.get_json()['AIID']
+    for i in Agent.query.filter_by(id=ai_id):
+        db.session.delete(i)
+    db.session.commit()
+    return json.dumps({'errcode': 0})
 
 
-@app.route('/get_or_create/<name>')
-def get_or_create(name):
-    raise NotImplementedError
-
-
-@app.route('/kill/<name>')
-def kill(name):
-    raise NotImplementedError
+@flask_method
+def empty(request):
+    db.session.query(Agent).delete()
+    db.session.commit()
+    return json.dumps({'errcode': 0})
--- dream/server/models.py
+++ dream/server/models.py
@@ -6,9 +6,9 @@ db = SQLAlchemy()
 
 class Agent(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), unique=True, nullable=False)
-    recv = db.Column(db.String(255), unique=True, nullable=False)
-    send = db.Column(db.String(255), unique=True, nullable=False)
+    type = db.Column(db.Integer, nullable=False)
+    name = db.Column(db.Integer, unique=True, nullable=False)
+    port = db.Column(db.Integer, unique=True, nullable=False)
 
     def __repr__(self):
         return '<Name %s>' % self.name
@@ -23,6 +23,17 @@ class Config(db.Model):
 
     def __repr__(self):
         return '<Key %s>' % self.key
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+
+class Quota(db.Model):
+    type = db.Column(db.String(255), primary_key=True)
+    maximum_quota = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return '<Type %s>' % self.type
 
     def as_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
