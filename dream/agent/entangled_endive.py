69daf5d82cebb31a767f9a39a6ff76cb4fb56d75
Icyblade Dai <icyblade.py@gmail.com>
2018-04-04 17:48:22 +0800

    add Entangled Endive

---
+++ dream/agent/entangled_endive.py
@@ -0,0 +1,208 @@
+import logging
+import traceback
+import gc
+
+import zmq
+
+from . import BaseAgent
+from ..bluff import BaseBluff
+from ..game.observation import Observation
+from ..handcard import BaseHandCard
+from ..logger import build_logger
+from ..policy.return_call import ReturnCall as Policy
+from ..style import BaseStyle
+from ..value import BaseValue
+
+
+class Agent(BaseAgent):
+    """Entangled Endive Texas Hold'em AI.
+
+    Parameters
+    --------
+    name: str
+        Name of agent.
+    port: int
+        Port to be binded.
+    level: int
+        Logging level. Default: logging.WARNING.
+    """
+    ev_range = 0.0
+
+    def __init__(self, name, port, level=logging.WARNING):
+        super(Agent, self).__init__()
+
+        self.name = name
+        self.logger = build_logger(f'{__name__}/{self.name}', level=level)
+
+        self._context = zmq.Context()
+        self._port = port
+        self._socket = self._context.socket(zmq.REP)
+        self._socket.bind(f'tcp://*:{port}')
+
+        self.policy = Policy()
+        self.value = BaseValue()
+        self.style = BaseStyle()
+        self.bluff = BaseBluff()
+        self.handcard = BaseHandCard()
+
+        self._token = None
+        self._request_id = None
+
+    @property
+    def token(self):
+        return self._token
+
+    @token.setter
+    def token(self, value):
+        if self._token is not None and self._token != value:
+            raise ValueError(f'Trying to change immutable token of agent {self.name}.')
+
+        self._token = value
+
+    @property
+    def request_id(self):
+        return self._request_id
+
+    @request_id.setter
+    def request_id(self, value):
+        if self._request_id is not None and self._request_id != value:
+            raise ValueError(f'Trying to change immutable request_id of agent {self.name}.')
+
+        self._request_id = value
+
+    def __del__(self):
+        self._socket.close()
+        self._context.destroy()
+
+    def observe(self):
+        """Get observation.
+
+        Returns
+        --------
+        obj
+            Incoming JSON message, e.g. new observation.
+        """
+        msg = self._socket.recv_json()
+        self.logger.debug(f'Receiving message: {msg}.')
+
+        return msg
+
+    def act(self, action):
+        """Do action.
+
+        Parameters
+        --------
+        action: instance of `dream.game.action.Action`
+            Action to be done.
+
+        Returns
+        --------
+        obj
+            Incoming JSON message returned by `action`.
+        """
+        msg = {
+            'RS': {
+                'token': self.token,
+                'requestID': self.request_id,
+                'data': {
+                    'playerAction': {
+                        'action': repr(action)
+                    }
+                }
+            }
+        }
+        self.logger.debug(f'Sending message: {msg}.')
+        msg = self._socket.send_json(msg)
+
+        return msg
+
+    def send_exception(self, exception):
+        """Send exception raised by AI.
+
+        Parameters
+        --------
+        exception: Exception
+            Raised exception.
+
+        Returns
+        --------
+        obj
+            Incoming JSON message returned by `exception`.
+        """
+        str_exception = str(exception)
+
+        if isinstance(exception, SystemExit):
+            msg = {'RS': {'errcode': 0, 'errmsg': str_exception}}
+            self.logger.info(f'SystemExit captured: {str_exception}.')
+            self.logger.debug(f'Sending exception: {msg}.')
+        else:
+            msg = {'RS': {'errcode': 1, 'errmsg': str_exception}}
+            self.logger.error(f'Exception raised: {str_exception}.')
+            self.logger.error(traceback.format_exc())
+            self.logger.debug(f'Sending exception: {msg}.')
+        msg = self._socket.send_json(msg)
+
+        return msg
+
+    def run(self):
+        """Run the agent."""
+        self.logger.info(f'Agent {self.name} started, binding at tcp://*:{self._port}.')
+
+        while True:
+            gc.collect()
+
+            msg = self.observe()
+            try:
+                self._validate_message(msg)
+                self.token = msg['RQ']['token']
+                self.request_id = msg['RQ']['requestID']
+                msg_type = self._get_message_type(msg)
+
+                if msg_type == 'PLAYERACTION':
+                    observation = Observation()
+                    observation.update_json(msg['RQ']['data'])
+                    action = self.policy.act(observation, None, None)
+                    self.logger.debug(f'Acting action: {action}.')
+                    self.act(action)
+                elif msg_type == 'DELETE':
+                    self.send_exception(SystemExit('Agent exits due to DELETE command'))
+                    return
+                else:
+                    self.send_exception(ValueError(f'Unknown message type: {msg_type}'))
+            except Exception as e:
+                self.send_exception(e)
+                continue
+
+    def _validate_message(self, msg):
+        """Validate message.
+
+        Parameters
+        --------
+        msg: obj
+            Message object to be validated.
+
+        Raises
+        --------
+        ValueError
+            If `msg` is invalid.
+        """
+        if 'RQ' not in msg or 'token' not in msg['RQ'] or 'requestID' not in msg['RQ']:
+            raise ValueError('Invalid message.')
+
+    def _get_message_type(self, msg):
+        """Determine incoming JSON message type.
+
+        Parameters
+        --------
+        msg: obj
+            Incoming JSON message.
+
+        Returns
+        --------
+        str
+            Message type. Possible values: PLAYERACTION, DELETE and UNKNOWN.
+        """
+        try:
+            return msg['RQ']['action']
+        except KeyError:
+            return 'UNKNOWN'
---
+++ tests/agent/test_entangled_endive.py
@@ -0,0 +1,143 @@
+upstream_send_message = [
+    {
+        'RQ': {
+            'action': 'PLAYERACTION',
+            'data': {
+                'playerAction': {
+                    'boardCards': '',
+                    'log': [
+                        'PokerStars Zoom Hand #189:  Hold\'em No Limit ($2/$4) - 2018/01/31 17:52:09 ET',
+                        'Table \'2_1\' 9-max Seat #1 is the button',
+                        'Seat 1: Test_006 ($1986 in chips)',
+                        'Seat 5: AI_005 ($2008 in chips)',
+                        'Test_006: posts small blind $2',
+                        'AI_005: posts big blind $4',
+                        '*** HOLE CARDS ***',
+                        'Dealt to bmlm [4h Qd]',
+                        'AI_005: checks',
+                        'Test_006: folds',
+                        '*** SUMMARY ***',
+                        'Total pot $6 | Rake $0',
+                        'Board []'
+                    ],
+                    'newCards': '',
+                    'player': {
+                        'AIID': '2156',
+                        'card': '4h Qd',
+                        'chips': '2008',
+                        'position': '4',
+                        'userID': '2400'
+                    },
+                    'pots': [{
+                        'pot': '0'
+                    }],
+                    'round': 'PREFLOP'
+                }
+            },
+            'requestID': '15',
+            'timeStamp': '2018-01-31 17:52:29',
+            'token': '869492cc3d74d5bc26f0c9633c3edafb'
+        }
+    },
+    {
+        'RQ': {
+            'token': '869492cc3d74d5bc26f0c9633c3edafb',
+            'requestID': '15',
+            'action': 'DELETE',
+        }
+    }
+]
+
+upstream_recv_message = [
+    {
+        'RS': {
+            'data': {
+                'playerAction': {
+                    'action': 'CALL'
+                }
+            },
+            'requestID': '15',
+            'token': '869492cc3d74d5bc26f0c9633c3edafb'
+        }
+    },
+    {
+        'RS': {
+            'errcode': 0,
+            'errmsg': 'Agent exits due to DELETE command'
+        }
+    },
+]
+
+ai_message = [
+    'Agent TestAgent started, binding at tcp://*:48546',
+    "Receiving message: {'RQ': {'action': 'PLAYERACTION',",
+    'Acting action: CALL',
+    (
+        "Sending message: {'RS': {'token': '869492cc3d74d5bc26f0c9633c3edafb', 'requestID': '15', "
+        "'data': {'playerAction': {'action': 'CALL'}}}}"
+    ),
+    "Receiving message: {'RQ': {'token': '869492cc3d74d5bc26f0c9633c3edafb', 'requestID': '15', 'action': 'DELETE'}}",
+    'SystemExit captured: Agent exits due to DELETE command',
+    "Sending exception: {'RS': {'errcode': 0, 'errmsg': 'Agent exits due to DELETE command'}}",
+]
+
+assert len(upstream_send_message) == len(upstream_recv_message)
+
+
+def upstream_thread(return_value, ip='127.0.0.1'):
+    try:
+        from time import sleep
+
+        import zmq
+
+        context = zmq.Context()
+        socket = context.socket(zmq.REQ)
+        socket.connect(f'tcp://{ip}:48546')
+        sleep(0.1)  # wait for binding
+
+        for send_message, recv_message in zip(upstream_send_message, upstream_recv_message):
+            socket.send_json(send_message)
+            sleep(0.1)
+            assert socket.recv_json() == recv_message
+            sleep(0.1)
+    except Exception as e:
+        return_value.append(e)
+
+
+def ai_thread(capsys, return_value):
+    import logging
+
+    from dream.agent.entangled_endive import Agent
+
+    agent = Agent('TestAgent', 48546, level=logging.DEBUG)
+    agent.run()
+
+    stdout, stderr = capsys.readouterr()
+    return_value.extend(stderr.splitlines())
+
+
+def test_ai(capsys):
+    from time import sleep
+    from threading import Thread
+
+    ai_return_value, upstream_return_value = [], []
+
+    threads = [
+        Thread(target=ai_thread, args=(capsys, ai_return_value)),
+        Thread(target=upstream_thread, args=(upstream_return_value,)),
+    ]
+
+    for i in threads:
+        i.daemon = True
+        i.start()
+        sleep(0.1)
+    for i in threads:
+        i.join(timeout=1)
+
+    for exception in upstream_return_value:
+        raise exception
+
+    assert len(ai_message) == len(ai_return_value)
+
+    for return_value, pattern in zip(ai_return_value, ai_message):
+        assert return_value.find(pattern) != -1
