8f206b8b2b73b199b4ca5d7d4d9f38323cc4469a
Icyblade Dai <icyblade.py@gmail.com>
2018-04-04 18:55:54 +0800

    make coloredlogs optional

--- dream/logger.py
+++ dream/logger.py
@@ -1,13 +1,15 @@
 import logging
 
-import coloredlogs
-
 
 def build_logger(name, level=logging.WARNING):
     logger = logging.getLogger(name)
     logger.handlers = []
     logger.setLevel(level)
 
-    coloredlogs.install(fmt='[%(levelname)s] %(asctime)s %(name)s: %(message)s', level=level)
+    try:
+        import coloredlogs
+        coloredlogs.install(fmt='[%(levelname)s] %(asctime)s %(name)s: %(message)s', level=level)
+    except ImportError:
+        pass
 
     return logger
