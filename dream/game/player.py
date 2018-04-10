cfa6b449e8a7ad3f5e79e7c742aedd8d1de6e4c0
Icyblade Dai <icyblade.py@gmail.com>
2018-04-10 18:11:05 +0800

    add __repr__ for dream.game.player.Player

--- dream/game/player.py
+++ dream/game/player.py
@@ -3,3 +3,6 @@ class Player(object):
         self.player_name = player_name
         self.seat_id = seat_id
         self.chips = chips
+
+    def __repr__(self):
+        return f'<Player {self.player_name} at seat {self.seat_id}>'
